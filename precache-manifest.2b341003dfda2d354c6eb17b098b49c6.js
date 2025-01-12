60511bef3dfd67c06e713335b7c86f288be990b3
aumirza <31508843+aumirza@users.noreply.github.com>
2025-01-12 18:57:52 +0000

    Deploying to gh-pages from @ aumirza/coming-soon-page-react@1972b5647f5b1e1fec5fa8b023bfd53c5f0c043e 🚀

---
+++ precache-manifest.2b341003dfda2d354c6eb17b098b49c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b055142d3298c11916a148ca0ffcb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6a67f672ffbf9ac45fc5",
+    "url": "/static/css/main.3c0d9940.chunk.css"
+  },
+  {
+    "revision": "877d0b28f5ebc4133a63",
+    "url": "/static/js/2.fdf9bc90.chunk.js"
+  },
+  {
+    "revision": "6a67f672ffbf9ac45fc5",
+    "url": "/static/js/main.7cb6d9fe.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
